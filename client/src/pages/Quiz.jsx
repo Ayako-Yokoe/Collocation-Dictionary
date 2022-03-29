f2a894e51ff41f6e3375ff2ca530046a7099a0cb
Ayako-Yokoe <niluvsuall@gmail.com>
2022-03-29 16:51:29 -0700

    add quiz page

--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -1,16 +1,57 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
+import Navbar from '../components/Navbar'
 
 
 const Container = styled.div`
-  /* display: flex;
+  width: 100vw;
+  height: auto;
+  text-align: center;
+`
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
   justify-content: center;
-  align-items: center; */
+  align-items: center;
+`
+const SearchField = styled.div`
+  flex: 1;
+  text-align: center;
+  margin: 2rem auto;
+  /* background-color: pink; */
+`
+const InputField = styled.input`
+  padding: 0.5rem 1rem;
+  font-size: 1.2rem;
+  width: 50%;
+  outline: none;
+`
+const Button = styled.button`
+  padding: 0.5rem 1rem;
+  font-size: 1.2rem;
+  cursor: pointer;
+`
+const SearchWord = styled.p`
+  font-size: 1.2rem;
+  padding: 2rem;
+`
+const AnswerField = styled.div`
+  flex: 2;
+  text-align: center;
+  margin: 2rem auto;
+  /* background-color: lightblue; */
+`
+const Answer = styled.p`
+  font-size: 1.2rem;
+  padding: 2rem;
 `
 
-
-// without mongodb? use direct access to api?
+const ResetButton = styled.button`
+    padding: 0.5rem 1rem;
+    font-size: 1.2rem;
+    cursor: pointer;
+`
 
 
 const Quiz = () => {
@@ -19,21 +60,23 @@ const Quiz = () => {
     id: "",
     collocation: "",
     examples: []
-}])
+  }])
   const [isLoading, setIsLoading] = useState(false)
-
-// console.log("restults collocation: " + results[0].collocation)
-
-
+  const [inputAnswer, setInputAnswer] = useState('')
   const [answer, setAnswer] = useState('')
   const [correct, setCorrect] = useState(false)
 
+  // console.log("search: " + search)
+  // console.log("results: " + results)
+  // console.log("inputAnswer: " + inputAnswer)
+  // console.log("answer: " + answer)
+  // console.log("correct: " + correct)
+  // console.log("=========================")
 
   const options = {
     method: 'GET',
     url: 'https://linguatools-english-collocations.p.rapidapi.com/bolls/',
-    // params: {lang: 'en', query: 'smoke', max_results: '15'},
-    params: {lang: 'en', query: `${search}`, max_results: '10'},
+    params: {lang: 'en', query: 'smoke', max_results: '15'},
     headers: {
       'x-rapidapi-host': process.env.REACT_APP_rapidapi_host,
       'x-rapidapi-key': process.env.REACT_APP_rapidapi_key,
@@ -49,45 +92,43 @@ const Quiz = () => {
       setIsLoading(true)
   }
 
-  // empty the input field
-
-
   const handleCheckAnswer = () => {
-    results.map(result => {
-      //result.collocation === answer ? setCorrect(true) : setCorrect(false)
-      result.collocation === answer ? console.log("true") : console.log("false")
+    setAnswer(inputAnswer)
+    setInputAnswer('')
 
-    })
+    const tempAnswer = inputAnswer
+    if(results === []) return
+    let obj = results.filter(r => r.collocation === tempAnswer)
+    obj.length > 0 ? setCorrect(true) : setCorrect(false)
+    
+    setAnswer('')
   }
 
+  const handleReset = () => {
+    setSearch('')
+    setResults([])
+    setInputAnswer('')
+    setAnswer('')
+  }
 
   return (
     <Container>
-      <div>
-        <input type="text" placeholder='search' onChange={(e) => setSearch(e.target.value)}  />
-        <button onClick={searchWord} >search</button>
+      <Navbar />
+      <Wrapper>
+      <SearchField>
+        <InputField type="text" value={search || ""} placeholder='search' onChange={(e) => setSearch(e.target.value)}  />
+        <Button onClick={searchWord} >search</Button>
+        <SearchWord>{search}</SearchWord>
+      </SearchField>
 
-        <p>{search}</p>
-
-        {/* {results && results.map(result =>  (
-          <p>Collocation: {result.collocation}</p>
-        ))} */}
-      </div>
-
-      <div>
-        {/* <input type='text' placeholder='type collocation' value={answer} onChange={(e) => setAnswer(e.target.value)} />
-        <button onClick={handleCheckAnswer}>check</button> */}
-
-        <input type='text' placeholder='type collocation' onChange={(e) => setAnswer(e.target.value)}  />
-        <button onClick={() => handleCheckAnswer()} >check</button> 
-        { correct ? <p>Correct</p> : <p>Let's try</p>}
-        
-
-        {/* {results.map(result => (
-            result.collocation === answer ? <p>correct</p> : <p>wrong</p>
-        ))} */}
-        
-      </div>
+      <AnswerField>
+        <InputField type='text' value={inputAnswer ? inputAnswer : ""} placeholder='type collocation' onChange={(e) => setInputAnswer(e.target.value)}  />
+        <Button onClick={handleCheckAnswer} >check</Button> 
+        { correct ? <Answer>Correct</Answer> : <Answer>Let's try</Answer>}
+      </AnswerField>
+ 
+      </Wrapper>
+      <ResetButton onClick={handleReset}>Reset</ResetButton>
     </Container>
   )
 }
