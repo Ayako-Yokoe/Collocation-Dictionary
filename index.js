a34fe7fc2ab52b7c5d03e2b3e7144574ae15a8ea
Ayako-Yokoe <niluvsuall@gmail.com>
2022-04-11 14:26:12 -0700

    make it better

--- index.js
+++ index.js
@@ -17,12 +17,17 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/wordlist', wordlistRouter)
 
-app.use(express.static(path.resolve(__dirname, './client/build')))
+// app.use(express.static(path.resolve(__dirname, './client/build')))
 
+// app.get('*', (req,res) => {
+//     res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+// })
 
-app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
-})
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 
 app.listen(process.env.PORT || 8000, () => {
